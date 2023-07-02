b7c94a2e50d981bd2ca7bdfb3527b751c819f7ed
TianHuijun <100184213+TianHuijun@users.noreply.github.com>
2023-07-03 01:07:46 +0700

    Update draw_vector_in_2d.py

--- draw_vector_in_2d.py
+++ draw_vector_in_2d.py
@@ -5,6 +5,8 @@ def draw_vector(vector, color, label):
     plt.text(vector[0] + 0.5, vector[1] + 0.5, label)
 
 def draw_vectors(vectors_dict):
+    min_x = min(v[0] for v in vectors_dict.values())
+    min_y = min(v[1] for v in vectors_dict.values())
     max_x = max(v[0] for v in vectors_dict.values())
     max_y = max(v[1] for v in vectors_dict.values())
 
@@ -13,10 +15,10 @@ def draw_vectors(vectors_dict):
     plt.gca().set_aspect('equal', adjustable='box')  # Set aspect ratio to equal
 
     # Set major and minor ticks to show grid lines
-    plt.gca().set_xticks(range(0, max_x + 2), minor=False)
-    plt.gca().set_yticks(range(0, max_y + 2), minor=False)
-    plt.gca().set_xticks(range(0, max_x + 1), minor=True)
-    plt.gca().set_yticks(range(0, max_y + 1), minor=True)
+    plt.gca().set_xticks(range(min_x - 2, max_x + 2), minor=False)
+    plt.gca().set_yticks(range(min_y - 2, max_y + 2), minor=False)
+    plt.gca().set_xticks(range(min_x - 1, max_x + 1), minor=True)
+    plt.gca().set_yticks(range(min_y - 1, max_y + 1), minor=True)
 
     plt.grid(which='both', color='gray', linestyle='-', linewidth=0.5)
     plt.grid(which='minor', alpha=0)  # Hide minor grid lines
@@ -29,10 +31,10 @@ def draw_vectors(vectors_dict):
     plt.show()
 
 vectors_dict = {
-    'a': [4, 8],
+    'u': [4, 8],
     'b': [9, 3],
-    'c': [6, 2],
-    'd': [5, 5]
+    'w1': [6, 2],
+    'w2': [-2,6]
 }
 
 draw_vectors(vectors_dict)
